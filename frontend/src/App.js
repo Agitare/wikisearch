69eec9585ef9d8e2f89c871c749c917f6e946f95
Agitare <61871277+Agitare@users.noreply.github.com>
2021-04-26 10:37:09 +0300

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,38 @@
-const express = require("express");
-var cors = require("cors");
-const { wikisearch } = require("./wikisearch");
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const app = express();
-app.use(cors());
+import "./App.css";
 
-app.listen(4000, () => {
-  console.log("Server running on port 4000");
-});
+function App() {
+  const [data, setData] = useState();
+  const [hakuTemp, setHakuTemp] = useState();
+  const [haku, setHaku] = useState();
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/wiki?haku=${haku}`)
+      .then(function (response) {
+        setData(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [haku]);
 
-app.get("/hello", (req, res, next) => {
-  res.end("Hello world!");
-});
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Haku"
+        onChange={(event) => setHakuTemp(event.target.value)}
+      />
+      <button onClick={(event) => setHaku(hakuTemp)}>Hae</button>
+      {data?.[0]?.title && <h1>{data[0].title}</h1>}
+      {data?.[0]?.thumbnail?.source && (
+        <img src={data?.[0]?.thumbnail?.source} />
+      )}
+      {data?.[0]?.extract && <p>{data[0].extract}</p>}
+    </div>
+  );
+}
 
-app.get("/wiki", (req, res, next) => {
-  wikisearch(req.query.haku, (error, data) => {
-    if (error) {
-      res.end(error);
-    } else {
-      res.json(data);
-    }
-  });
-});
+export default App;
